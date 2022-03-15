9a0d5c4847b92bb49c40f2336a13d0eadbcd27e1
Muhammad Saad <saad992.sm@gmail.com>
2022-03-15 21:04:07 +0500

    complete functionality till user settings for sos on volume and shake

---
+++ app/src/main/java/com/example/crimetracker/EmergencyNumber.java
@@ -0,0 +1,14 @@
+package com.example.crimetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmergencyNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_number);
+    }
+}
---
+++ app/src/main/java/com/example/crimetracker/SettingsPreference.java
@@ -0,0 +1,2 @@
+package com.example.crimetracker;public class SettingsPreference {
+}
